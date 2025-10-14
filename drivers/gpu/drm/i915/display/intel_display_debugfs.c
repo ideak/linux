45d793e9b9a485fa9a86ce9f44227f84880da772
Imre Deak <imre.deak@intel.com>
2025-10-14 17:42:03 +0300

    debug
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_debugfs.c
+++ drivers/gpu/drm/i915/display/intel_display_debugfs.c
@@ -562,6 +562,13 @@ static void intel_crtc_info(struct seq_file *m, struct intel_crtc *crtc)
 	seq_printf(m, "\tport_clock=%d, lane_count=%d\n",
 		   crtc_state->port_clock, crtc_state->lane_count);
 
+	seq_printf(m, "\tport_clock=%d, lane_count=%d\n",
+		   crtc_state->port_clock, crtc_state->lane_count);
+
+	drm_printf(&p, "fec: %s, enhanced framing: %s\n",
+		   str_enabled_disabled(crtc_state->fec_enable),
+		   str_enabled_disabled(crtc_state->enhanced_framing));
+
 	intel_scaler_info(m, crtc);
 
 	if (crtc_state->joiner_pipes)
