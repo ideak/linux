2e3bdef7d8220b6d948bba979d6e86e85a368ac7
Imre Deak <imre.deak@intel.com>
2025-11-15 01:11:15 +0200

    drm/i915: Refer to HIP register regions as HIP segments
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_hip_reg_defs.h
+++ drivers/gpu/drm/i915/display/intel_hip_reg_defs.h
@@ -15,34 +15,33 @@ struct intel_dkl_phy_reg {
 	u32 bank_idx:4;
 };
 
-#define _DKL_PHY1_BASE					0x168000
-#define _DKL_PHY2_BASE					0x169000
-#define _DKL_PHY3_BASE					0x16A000
-#define _DKL_PHY4_BASE					0x16B000
-#define _DKL_PHY5_BASE					0x16C000
-#define _DKL_PHY6_BASE					0x16D000
+#define _HIP_REG_SEG0_BASE				0x168000
+#define _HIP_REG_SEG1_BASE				0x169000
 
-#define DKL_REG_TC_PORT(__reg) \
-	(TC_PORT_1 + ((__reg).reg - _DKL_PHY1_BASE) / (_DKL_PHY2_BASE - _DKL_PHY1_BASE))
+#define HIP_REG_SEG_NUM					8
 
 /* DEKEL PHY MMIO Address = Phy base + (internal address & ~index_mask) */
 #define DKL_REG_MMIO(__reg)				_MMIO((__reg).reg)
 
-#define _DKL_REG_PHY_BASE(tc_port)			_PORT(tc_port, \
-							      _DKL_PHY1_BASE, \
-							      _DKL_PHY2_BASE)
+#define HIP_REG_SEG_IDX(__reg) \
+	(((__reg).reg - _HIP_REG_SEG0_BASE) / (_HIP_REG_SEG1_BASE - _HIP_REG_SEG0_BASE))
 
-#define _DKL_BANK_SHIFT					12
-#define _DKL_REG_BANK_OFFSET(phy_offset) \
-	((phy_offset) & ((1 << _DKL_BANK_SHIFT) - 1))
-#define _DKL_REG_BANK_IDX(phy_offset) \
-	(((phy_offset) >> _DKL_BANK_SHIFT) & 0xf)
 
-#define _DKL_REG(tc_port, phy_offset)	\
+#define _HIP_REG_SEG_BASE(__seg_idx)			_PORT(__seg_idx, \
+							      _HIP_REG_SEG0_BASE, \
+							      _HIP_REG_SEG1_BASE)
+
+#define _HIP_REG_SEG_BANK_SHIFT				12
+#define _HIP_REG_SEG_BANK_OFFSET(__offset) \
+	((__offset) & ((1 << _HIP_REG_SEG_BANK_SHIFT) - 1))
+#define _HIP_REG_SEG_BANK_IDX(__offset) \
+	(((__offset) >> _HIP_REG_SEG_BANK_SHIFT) & 0xf)
+
+#define _DKL_REG(__seg_idx, __seg_offset)	\
 	((const struct intel_dkl_phy_reg) { \
-		.reg = _DKL_REG_PHY_BASE(tc_port) + \
-		       _DKL_REG_BANK_OFFSET(phy_offset), \
-		.bank_idx = _DKL_REG_BANK_IDX(phy_offset), \
+		.reg = _HIP_REG_SEG_BASE(__seg_idx) + \
+		       _HIP_REG_SEG_BANK_OFFSET(__seg_offset), \
+		.bank_idx = _HIP_REG_SEG_BANK_IDX(__seg_offset), \
 	})
 
 #endif /* __INTEL_HIP_REG_DEFS_H__ */
