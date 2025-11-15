963ccc3905b62067e137ab06b3af6379e1911bf5
Imre Deak <imre.deak@intel.com>
2025-11-15 05:40:21 +0200

    drm/i915: Rename dkl_phy_set_hip_idx to hip_reg_set_bank_idx
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_dkl_phy.c
+++ drivers/gpu/drm/i915/display/intel_dkl_phy.c
@@ -35,7 +35,7 @@ void intel_dkl_phy_init(struct intel_display *display)
 }
 
 static void
-dkl_phy_set_hip_idx(struct intel_display *display, struct intel_dkl_phy_reg reg)
+hip_reg_set_bank_idx(struct intel_display *display, struct intel_dkl_phy_reg reg)
 {
 	int seg_idx = HIP_REG_SEG_IDX(reg);
 
@@ -64,7 +64,7 @@ intel_dkl_phy_read(struct intel_display *display, struct intel_dkl_phy_reg reg)
 
 	spin_lock(&display->dkl.phy_lock);
 
-	dkl_phy_set_hip_idx(display, reg);
+	hip_reg_set_bank_idx(display, reg);
 	val = intel_de_read(display, DKL_REG_MMIO(reg));
 
 	spin_unlock(&display->dkl.phy_lock);
@@ -85,7 +85,7 @@ intel_dkl_phy_write(struct intel_display *display, struct intel_dkl_phy_reg reg,
 {
 	spin_lock(&display->dkl.phy_lock);
 
-	dkl_phy_set_hip_idx(display, reg);
+	hip_reg_set_bank_idx(display, reg);
 	intel_de_write(display, DKL_REG_MMIO(reg), val);
 
 	spin_unlock(&display->dkl.phy_lock);
@@ -106,7 +106,7 @@ intel_dkl_phy_rmw(struct intel_display *display, struct intel_dkl_phy_reg reg, u
 {
 	spin_lock(&display->dkl.phy_lock);
 
-	dkl_phy_set_hip_idx(display, reg);
+	hip_reg_set_bank_idx(display, reg);
 	intel_de_rmw(display, DKL_REG_MMIO(reg), clear, set);
 
 	spin_unlock(&display->dkl.phy_lock);
@@ -124,7 +124,7 @@ intel_dkl_phy_posting_read(struct intel_display *display, struct intel_dkl_phy_r
 {
 	spin_lock(&display->dkl.phy_lock);
 
-	dkl_phy_set_hip_idx(display, reg);
+	hip_reg_set_bank_idx(display, reg);
 	intel_de_posting_read(display, DKL_REG_MMIO(reg));
 
 	spin_unlock(&display->dkl.phy_lock);
