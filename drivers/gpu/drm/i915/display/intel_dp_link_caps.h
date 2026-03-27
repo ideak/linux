864d71da3ec9d0b8f5e4e8715976d58e026fcc5f
Imre Deak <imre.deak@intel.com>
2026-03-27 22:28:26 +0200

    drm/i915/dp_link_caps: Use max common lane count from link_caps
    Convert all users of intel_dp_max_common_lane_count() to query the
    maximum common lane count via the link capability API, in common with
    the link rate queries.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp_link_caps.h
+++ drivers/gpu/drm/i915/display/intel_dp_link_caps.h
@@ -19,6 +19,7 @@ int intel_dp_max_common_rate(struct intel_dp *intel_dp);
 int intel_dp_link_caps_num_common_rates(struct intel_dp_link_caps *link_caps);
 void intel_dp_link_caps_all_common_rates(struct intel_dp_link_caps *link_caps,
 					 const int **rates, int *num_rates);
+int intel_dp_link_caps_max_common_lane_count(struct intel_dp_link_caps *link_caps);
 
 void intel_dp_link_caps_get_forced_params(struct intel_dp_link_caps *link_caps,
 					  struct intel_dp_link_config *forced_params);
