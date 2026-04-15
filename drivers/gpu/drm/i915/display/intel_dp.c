a06c0b50098126293d4ff4b6dfa757b54676e8ad
Imre Deak <imre.deak@intel.com>
2026-04-15 11:06:38 +0300

    drm/i915/dp: Simplify the modeset max link rate limit computation
    intel_dp_link_caps_get_max_limits() already accounts for forced
    parameters, so no separate adjustment is needed during modeset link
    configuration.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -356,16 +356,10 @@ int intel_dp_max_lane_count(struct intel_dp *intel_dp)
 {
 	struct intel_dp_link_caps *link_caps = intel_dp->link.caps;
 	struct intel_dp_link_config max_link_limits;
-	struct intel_dp_link_config forced_params;
 	int lane_count;
 
 	intel_dp_link_caps_get_max_limits(link_caps, &max_link_limits);
-	intel_dp_link_caps_get_forced_params(link_caps, &forced_params);
-
-	if (forced_params.lane_count)
-		lane_count = forced_params.lane_count;
-	else
-		lane_count = max_link_limits.lane_count;
+	lane_count = max_link_limits.lane_count;
 
 	switch (lane_count) {
 	case 1:
@@ -1550,12 +1544,6 @@ intel_dp_max_link_rate(struct intel_dp *intel_dp)
 {
 	struct intel_dp_link_caps *link_caps = intel_dp->link.caps;
 	struct intel_dp_link_config max_link_limits;
-	struct intel_dp_link_config forced_params;
-
-	intel_dp_link_caps_get_forced_params(link_caps, &forced_params);
-
-	if (forced_params.rate)
-		return forced_params.rate;
 
 	intel_dp_link_caps_get_max_limits(link_caps, &max_link_limits);
 
