5f08c27e7aa51cf7d656457c3c8ad459ce14234e
Imre Deak <imre.deak@intel.com>
2026-04-17 06:45:29 +0300

    drm/i915/dp_link_training: Factor out link training state reset helper
    Factor out the link training state reset into a helper in
    intel_dp_link_training.c to prepare for isolating the link training state
    from the generic DP code.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp_link_training.c
+++ drivers/gpu/drm/i915/display/intel_dp_link_training.c
@@ -2222,6 +2222,14 @@ void intel_dp_link_training_debugfs_add(struct intel_connector *connector)
 			    connector, &i915_dp_link_retrain_disabled_fops);
 }
 
+void intel_dp_link_training_reset(struct intel_dp_link_training *link_training)
+{
+	struct intel_dp *intel_dp = link_training->dp;
+
+	intel_dp->link.retrain_disabled = false;
+	intel_dp->link.seq_train_failures = 0;
+}
+
 /**
  * intel_dp_link_training_init - allocate and initialize link training state
  * @intel_dp: DP encoder state
