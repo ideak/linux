b76afa4f64794a2d1e9d642e4d1d8d50005e6958
Imre Deak <imre.deak@intel.com>
2026-04-27 15:25:19 +0300

    drm/i915/dp_link_caps: Add reset and merge update modes
    Add update modes to intel_dp_link_caps_update() to distinguish between
    resetting link capability state for a new sink and merging newly read
    capabilities into existing state.
    
    Reset mode is used when a new sink is connected, after
    intel_dp_reset_link_params() is called. Merge mode is used when an
    already connected sink reports a capability change, preserving disabled
    configurations that are still present after the update.
    
    This patch only implements the reset mode, by resetting the maximum link
    limits, the logic for merge behavior will be added by follow-up changes.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -686,14 +686,21 @@ static void intel_dp_get_common_rates(struct intel_dp *intel_dp,
 /* Return %true if any supported or maximum link param changed. */
 static bool intel_dp_set_common_link_params(struct intel_dp *intel_dp)
 {
+	enum intel_dp_link_caps_update_mode update_mode =
+		INTEL_DP_LINK_CAPS_UPDATE_MERGE;
 	int num_common_rates;
 	int common_rates[DP_MAX_SUPPORTED_RATES];
 	bool link_params_changed = false;
 
 	intel_dp_get_common_rates(intel_dp, common_rates, &num_common_rates);
+
+	if (intel_dp->reset_link_params)
+		update_mode = INTEL_DP_LINK_CAPS_UPDATE_RESET;
+
 	if (intel_dp_link_caps_update(intel_dp->link.caps,
 				      common_rates, num_common_rates,
-				      intel_dp_max_common_lane_count(intel_dp)))
+				      intel_dp_max_common_lane_count(intel_dp),
+				      update_mode))
 		link_params_changed = true;
 
 	return link_params_changed;
